da4dc80601e08fe3a6cde6161eb91280db7e4d93
kaizerakafollen <30346116+kaizerakafollen@users.noreply.github.com>
2017-07-21 16:15:21 +0300

    добавил файлы
    загрузил самую первую версию, окончен на 25 %

---
+++ js/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+})
